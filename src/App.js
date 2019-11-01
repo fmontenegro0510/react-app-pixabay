4438d8fd6df426be2628e37d2cce110ee18ba1f1
fmontenegro0510 <franciscomontenegro22@gmail.com>
2019-11-01 18:45:39 -0300

    First Install and first steps

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="app container">
+      <div className="jumbotron">
+        <div className="lead text-center title">
+          Buscador de Imagenes
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
